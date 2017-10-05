b9940cf796c6d134ae7ca4ece443f6cf379b34c6
jacob <jacob@debian.com>
2017-10-05 11:15:24 -0400

    Commented descriptions of functions in main.cc

--- Sample_student_project/main.cc
+++ Sample_student_project/main.cc
@@ -2,6 +2,7 @@
 // CS2401
 // November 23, 2015
 // Project7
+// Edited by Jacob Handley 10/5/2017
 
 #include "game.h"
 #include "othello.h"
@@ -10,7 +11,12 @@ using namespace main_savitch_14;
 
 int main()
 {
+	//Creates an Othello class called "theGame"
 	Othello theGame;
+
+	//Resets the game and returns all pieces to the starting position
 	theGame.restart();
+
+	//Calls the loop that runs until the game is over
 	theGame.play();
 }
